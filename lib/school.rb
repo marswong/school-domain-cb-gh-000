e837413d7f764315d6b6b854dd2e5f4478be952b
Mars Wong <marswong618@gmail.com>
2018-01-30 02:30:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if !roster[grade]
+    @roster[grade] << name
+  end
+
+  def grade(n)
+    @roster[n]
+  end
+
+  def sort
+    puts @roster.sort.Class
+  end
+end
